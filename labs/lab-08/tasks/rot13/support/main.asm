5ad0ef1fbdd2a3add0d245f4f9eaca467285fe35
teodutu <teodutu@users.noreply.github.com>
2026-05-03 17:50:48 +0000

    deploy: dc9afafb9fa4b539fcfd0bbd97282265508b96c5

---
+++ labs/lab-08/tasks/rot13/support/main.asm
@@ -0,0 +1,27 @@
+section .data
+    mystring db "lorem", 0, "ipsum", 0, "dolor", 0
+    length   dq ($ - mystring)
+
+    after_fmt  db "After: '%s'", 10, 0
+
+section .text
+extern printf
+extern rot13
+global main
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    mov rax, [length]
+
+    mov rdi, mystring
+    mov rsi, rax
+    call rot13
+
+    mov rdi, after_fmt
+    mov rsi, mystring
+    call printf
+
+    leave
+    ret
